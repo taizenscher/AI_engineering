c074387e651b58fbbd07943d548ab6d4518e8564
taizenscher <taizenscher@gmail.com>
2026-07-20 10:52:30 -0300

    feat(message): AgentResponse refactorization

--- base_proyect_cli_v1/models/message.py
+++ base_proyect_cli_v1/models/message.py
@@ -1,5 +1,8 @@
 
-class AgentResponse:
-    def __init__(self, message:str=None)->None:
-        self.message:str = message
+class Message:
+    def __init__(self, message:str=None, role:str=None)->None:
+        self.id:int = None
+        self.role:str = role
+        self.content:str = message
         self.metadata:str = None
+
